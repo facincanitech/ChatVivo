13c09e33f674f0deed7e2a0ac361cd13ae6269cc
pezzarine <facincanitech@gmail.com>
2026-08-28 15:31:53 -0300

    Rename app to Ferus

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -357,7 +357,7 @@ export function ChatList({
   return (
     <section className="chats">
       <div className="top">
-        <div className="brand">Eros</div>
+        <div className="brand">Ferus</div>
       </div>
 
       <div className="search-wrap">
